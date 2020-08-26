9e41d436fc3288acc62ab7e0a9b98777b8629fb8
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-08-26 00:04:50 -0300

    Estrutura padão

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, ()=> {
     console.log('Server started');
