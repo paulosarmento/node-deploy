81ca6fb8496b1fc4fdbf52bce1e1d7c1f607bb5a
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-09-22 12:04:42 -0300

    Usando controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,29 +1,13 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import SessionsController from '../controllers/SessionsController';
 
 
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController()
 
 
-sessionsRouter.post('/', async (request, response) => {
-
-    const { email, password } = request.body;
-
-    const authenticateUser = container.resolve(AuthenticateUserService);
-
-    const { user, token } = await authenticateUser.execute({
-        email,
-        password,
-      });
-
-      delete user.password;
-
-      return response.json({ user, token });
-
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
