91146855fa201f56b3721e9b7aec52d5bdca5305
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-09-29 08:37:33 -0300

    Salvando tokens no banco

---
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
+
+
+
+// Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
+
+const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
+
+passwordRouter.post('/forgot', forgotPasswordController.create);
+passwordRouter.post('/reset', resetPasswordController.create);
+
+export default passwordRouter;
