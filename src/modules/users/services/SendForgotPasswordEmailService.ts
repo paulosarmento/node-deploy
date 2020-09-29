c48cefb898313a36b287eed0390c0bbf00013e82
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-09-29 11:08:23 -0300

    Emails em desenvolvimento

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -33,9 +33,9 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha reccebido',);
+    await this.mailProvider.sendMail(email, `Pedido de recuperação de senha reccebido: ${token}`,);
   }
 }
 
