216d9fdbb08a9dac4e42d6f37db4f89f5c4edd42
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-10-11 00:38:32 -0300

    Upload de arquivo para Amazon S3 - ok

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,4 +1,5 @@
 import{ Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn  } from 'typeorm';
+import uploadConfig from '@config/upload';
 
 import { Exclude, Expose } from 'class-transformer';
 
@@ -28,9 +29,19 @@ class User {
 
   @Expose({ name: 'avatar_url'})
   getAvatarUrl(): string | null{
-    return this.avatar
-    ? `${process.env.APP_API_URL}/files/${this.avatar}`
-    : null;
+    if(!this.avatar){
+      return null;
+    }
+
+    switch(uploadConfig.driver){
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
+
   }
 
 }
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from './models/IStorageProvider';
 import DiskStorageProvider from './implementations/DiskStorageProvider';
@@ -12,5 +13,5 @@ const providers = {
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
