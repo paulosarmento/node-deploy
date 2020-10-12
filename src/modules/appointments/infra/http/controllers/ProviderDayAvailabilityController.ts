e14bf18e257c9468c1ed41c95d33b5a4fafe53ab
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-10-12 08:38:28 -0300

    Utilizando Query Params

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -9,15 +9,15 @@ export default class ProviderDayAvailabilityController {
 
     const { provider_id } = request.params;
 
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderDayAvailability = container.resolve(ListProviderDayAvailabilityService);
 
     const availability = await listProviderDayAvailability.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -9,14 +9,14 @@ export default class ProviderMonthAvailabilityController {
 
     const { provider_id } = request.params;
 
-    const {  month, year } = request.body;
+    const {  month, year } = request.query;
 
     const listProviderMonthAvailability = container.resolve(ListProviderMonthAvailabilityService);
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
