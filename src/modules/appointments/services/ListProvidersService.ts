b4443af2ab3dbd6f4bdba184d2d87326048b37ff
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-11-10 00:39:37 -0300

    Ajustes

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -21,13 +22,15 @@ class ListProvidersService {
   public async execute({ user_id }: IRequest): Promise<User[]>{
 
     let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+    //! Limpar cache da aplicação
+    // let users;
 
     if(!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
 
